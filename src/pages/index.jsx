f52b0443eb57f3770303a62eebb510e4487cfcfe
anthonyc-a <anthonyc.animba@gmail.com>
2022-05-16 14:59:05 +0100

    -

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,16 +3,12 @@ import SectionFive from "../components/Home/SectionFive"
 import SectionFour from "../components/Home/SectionFour"
 import SectionOne from "../components/Home/SectionOne"
 import SectionSix from "../components/Home/SectionSix"
-import gsap from "gsap"
-import { ScrollTrigger } from "gsap/dist/ScrollTrigger"
 import SectionThree from "../components/Home/SectionThree"
 import SectionTwo from "../components/Home/SectionTwo"
 import Layout from "../components/Layout/Layout"
 import indexStyles from "../styles/Index.scss"
 
 export default function Home() {
-  gsap.registerPlugin(ScrollTrigger)
-
   const ref = React.useRef()
   const [pageCounter, setPageCounter] = React.useState(1)
 
@@ -21,27 +17,27 @@ export default function Home() {
       setPageCounter(0)
       console.log(pageCounter)
 
-      if (window.pageYOffset >= ref.current.offsetHeight / 6) {
+      if (window.pageYOffset >= ref.current.offsetHeight / 3) {
         setPageCounter(1)
         console.log(pageCounter)
       }
 
-      if (window.pageYOffset >= ref.current.offsetHeight / 6 * 2) {
+      if (window.pageYOffset >= ref.current.offsetHeight / 3 * 2) {
         setPageCounter(2)
         console.log(pageCounter)
       }
 
-      if (window.pageYOffset >= ref.current.offsetHeight / 6 * 3) {
+      if (window.pageYOffset >= ref.current.offsetHeight / 3 * 3) {
         setPageCounter(3)
         console.log(pageCounter)
       }
 
-      if (window.pageYOffset >= ref.current.offsetHeight / 6 * 4) {
+      if (window.pageYOffset >= ref.current.offsetHeight / 3 * 4) {
         setPageCounter(4)
         console.log(pageCounter)
       }
       
-      if (window.pageYOffset >= ref.current.offsetHeight / 6 * 5) {
+      if (window.pageYOffset >= ref.current.offsetHeight / 3 * 5) {
         setPageCounter(5)
         console.log(pageCounter)
       }
